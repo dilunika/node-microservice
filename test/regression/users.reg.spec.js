14e5cdd1c9b4ee5f58247390319ba522ad1088a4
Dilunika <kasun.dilunika@contactenergy.co.nz>
2017-06-28 17:01:07 +1200

    Imporved regression tests.

--- test/regression/users.reg.spec.js
+++ test/regression/users.reg.spec.js
@@ -3,28 +3,58 @@
  */
 const request = require('supertest');
 const assert  = require('chai').assert;
+const faker = require('faker');
 
-const api = request('http://localhost:5501/users');
-const user = {
-    firstName: 'Kasun',
-    lastName: 'Dilunika',
-    email: 'k@y.com',
-    password: 'secret',
-    role: 1
-};
 
-it('Create new user', () => {
-    return api.post('/')
-        .send(user)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(201)
-        .then(res => assertUserIsCreated(res.body.user));
+const api = request('http://localhost:5501');
+
+describe('/users API', () => {
+
+    it('Create new user', () => {
+
+        const userData = newUser();
+        return api.post('/users')
+            .send(userData)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .then(res => assertUserIsCreated(res.body.user, userData));
+    });
+
+    it('Get user by id', () => {
+
+        const userData = newUser();
+        return api.post('/users')
+            .send(userData)
+            .then(res => assertSameUserIsFetched(res))
+    });
 });
 
-function assertUserIsCreated(createdUser) {
+function assertUserIsCreated(createdUser, user) {
 
-    console.log(JSON.stringify(createdUser));
     assert.isNotNull(createdUser.id);
     assert(createdUser.email, user.email);
+}
+
+function newUser() {
+
+    return {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: 1
+    };
+}
+
+function assertSameUserIsFetched(res) {
+
+    console.log(res.body);
+    const user = res.body.user;
+    return api.get(res.body._links.self.href)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(res => {
+           assert(res.body.user.id, user.id);
+        });
 }
